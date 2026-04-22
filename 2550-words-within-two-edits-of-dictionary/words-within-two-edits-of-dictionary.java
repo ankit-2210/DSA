6fbbddf9ee6f8d5ed48d5e1b406b54b4cb06566a
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-04-22 10:28:12 +0530

    Time: 9 ms (48.53%) | Memory: 47.1 MB (23.53%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -1,26 +1,73 @@
 class Solution{
-    void solve(String w, String s, int[] t){
-        for(int i=0; i<w.length(); i++){
-            if(w.charAt(i) != s.charAt(i))
-                t[0]++;
+    static final int ALPHABET_SIZE=26;
+    class TrieNode{
+        TrieNode[] children=new TrieNode[ALPHABET_SIZE];
+        boolean isEnd;
+
+        TrieNode(){
+            isEnd=false;
+            for(int i=0; i<ALPHABET_SIZE; i++){
+                children[i]=null;
+            }
         }
     }
+
+    TrieNode root;
+    public Solution(){
+        root=new TrieNode();
+    }
+
+    public void insert(String w){
+        TrieNode pCrawl=root;
+        for(char ch: w.toCharArray()){
+            int idx=ch-'a';
+            if(pCrawl.children[idx] == null){
+                pCrawl.children[idx]=new TrieNode();
+            }
+            pCrawl=pCrawl.children[idx];
+        }
+        pCrawl.isEnd=true;
+    }
+
+    boolean solve(int idx, String w, int ed, TrieNode node){
+        if(ed>2)
+            return false;
+        if(idx == w.length()){
+            if(node.isEnd)
+                return true;
+            return false;
+        }
+
+        for(int i=0; i<26; i++){
+            if(node.children[i] != null){
+                if(i == w.charAt(idx)-'a'){
+                    if(solve(idx+1, w, ed, node.children[i]))
+                        return true;
+                }
+                else{
+                    if(solve(idx+1, w, ed+1, node.children[i]))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 
     public List<String> twoEditWords(String[] q, String[] d){
         int n=q.length;
         int m=d.length;
 
+        TrieNode pCrawl=root;
+        for(String it: d){
+            insert(it);
+        }
+
         List<String> res=new ArrayList<>();
         for(String it: q){
-            String w=it;
-            for(int i=0; i<m; i++){
-                int[] t=new int[1];
-                String s=d[i];
-                solve(w, s, t);
-                if(t[0]<=2){
-                    res.add(w);
-                    break;
-                }
+            if(solve(0, it, 0, pCrawl)){
+                res.add(it);
             }
         }
 
