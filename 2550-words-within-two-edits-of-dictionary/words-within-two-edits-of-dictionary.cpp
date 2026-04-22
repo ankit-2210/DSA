3fbeb65d683c20742f2c7b783cf7dfca651cff0d
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-04-22 10:18:00 +0530

    Time: 16 ms (33.16%) | Memory: 34.7 MB (6.42%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,31 +1,78 @@
 class Solution{
-public:
-    void solve(string &w, string &s, int &t){
-        for(int i=0; i<w.size(); i++){
-            if(w[i] != s[i])
-                t++;
+    static const int ALPHABET_SIZE=26;
+    struct TrieNode{
+        struct TrieNode *children[ALPHABET_SIZE];
+        bool isEnd;
+    };
+
+    struct TrieNode *root;
+    struct TrieNode *getNode(){
+        struct TrieNode *pNode=new TrieNode();
+        pNode->isEnd=false;
+        for(int i=0; i<ALPHABET_SIZE; i++){
+            pNode->children[i]=NULL;
         }
+        return pNode;
+    }
+
+public:
+    Solution(){
+        root=getNode();
+    }
+
+    void insert(string &w){
+        struct TrieNode *pCrawl=root;
+        for(auto ch: w){
+            int idx=ch-'a';
+            if(!pCrawl->children[idx]){
+                pCrawl->children[idx]=getNode();
+            }
+            pCrawl=pCrawl->children[idx];
+        }
+        pCrawl->isEnd=true;
+    }
+
+    bool solve(int idx, string &w, int ed, TrieNode *&node){
+        if(ed>2)
+            return false;
+        if(idx == w.size()){
+            if(node->isEnd)
+                return true;
+            return false;
+        }
+
+        for(int i=0; i<26; i++){
+            if(node->children[i]){
+                if(i == w[idx]-'a'){
+                    if(solve(idx+1, w, ed, node->children[i]))
+                        return true;
+                }
+                else{
+                    if(solve(idx+1, w, ed+1, node->children[i]))
+                        return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     vector<string> twoEditWords(vector<string> &q, vector<string> &d){
         int n=q.size();
         int m=d.size();
 
+        struct TrieNode *pCrawl=root;
+        for(auto it: d){
+            insert(it);
+        }
+
         vector<string> res;
         for(auto it: q){
-            string w=it;
-            for(int i=0; i<m; i++){
-                int t=0;
-                string s=d[i];
-                solve(w, s, t);
-                if(t<=2){
-                    res.push_back(w);
-                    break;
-                }
+            if(solve(0, it, 0, pCrawl)){
+                res.push_back(it);
             }
         }
 
         return res;
-
     }
 };
